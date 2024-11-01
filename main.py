3a793dbdd319064f5b0df64dd9b54f0c110eeca8
alim2709 <alimselemetow@gmail.com>
2024-11-01 23:56:48 +0100

    Include social_media_router to app

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from typing import Union
 
 from fastapi import FastAPI
 from app.users.routers import auth_router
+from app.social_media.routers import social_media_router
 
 app = FastAPI()
 
 app.include_router(auth_router)
+app.include_router(social_media_router)
 @app.get("/")
 def read_root():
     return {"Hello": "World how are you"}
