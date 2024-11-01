bd14213aa8401aab2da7083920ed931edb592f05
alim2709 <alimselemetow@gmail.com>
2024-11-01 13:43:49 +0100

    Implement first routers like create_user_account() & login_users()

---
+++ app/users/routers.py
@@ -0,0 +1,77 @@
+from datetime import timedelta
+
+from app.users.auth import get_password_hash, verify_password, create_access_token
+from app.config import settings
+from fastapi import APIRouter, HTTPException, status
+from fastapi.responses import JSONResponse
+from app.users.service import UserService
+from app.users.schemas import SUserCreateModel, SUserModel, SUserLoginModel
+
+auth_router = APIRouter()
+user_service = UserService()
+
+
+@auth_router.post("/signup", response_model=SUserModel)
+async def create_user_account(user_data: SUserCreateModel):
+    email = user_data.email
+
+    exist_user = await user_service.user_exists(email=email)
+    if exist_user:
+        raise HTTPException(
+            status_code = status.HTTP_409_CONFLICT,
+            detail = "User is already registered"
+        )
+    hashed_password = get_password_hash(user_data.password)
+
+    new_user = await user_service.create_user(email=email, hashed_password=hashed_password, username=user_data.username)
+
+    return SUserModel(
+        username=new_user.username,
+        email=new_user.email,
+        auto_reply_enabled=new_user.auto_reply_enabled,
+        auto_reply_delay=new_user.auto_reply_delay
+    )
+
+
+@auth_router.post("/login")
+async def login_users(user_login_data: SUserLoginModel):
+
+    email = user_login_data.email
+    password = user_login_data.password
+
+    user = await user_service.get_user_by_email(email=email)
+    if user:
+        password_valid = verify_password(password, user.hashed_password)
+        if password_valid:
+            access_token = create_access_token(
+                user_data={
+                    "email": user.email,
+                    "username": user.username,
+                    "id": str(user.id),
+                }
+            )
+
+            refresh_token = create_access_token(
+                user_data={
+                    "email": user.email,
+                    "username": user.username,
+                    "id": str(user.id),
+                },
+                refresh=True,
+                expire = timedelta(days=settings.REFRESH_TOKEN_EXPIRE_DAYS),
+            )
+
+            return JSONResponse(
+                content={
+                    "message": "You are now logged in",
+                    "access_token": access_token,
+                    "refresh_token": refresh_token,
+                }
+            )
+    else:
+        return JSONResponse(
+            status_code = status.HTTP_401_UNAUTHORIZED,
+            content = {
+                "message": "Invalid email or password",
+            }
+        )
