b1788b24e19b039c61b1ce8ec8ffe9ffdb9254af
alim2709 <alimselemetow@gmail.com>
2024-11-01 13:37:58 +0100

    Include auth_router into main app

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from typing import Union
 
 from fastapi import FastAPI
+from app.users.routers import auth_router
 
 app = FastAPI()
 
-
+app.include_router(auth_router)
 @app.get("/")
 def read_root():
     return {"Hello": "World how are you"}
