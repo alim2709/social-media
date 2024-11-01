eb5769d9c18e17226affb80b19c8f7f3f8b4372b
alim2709 <alimselemetow@gmail.com>
2024-11-01 13:39:01 +0100

    Refactor main settings at config.py file

--- app/config.py
+++ app/config.py
@@ -6,6 +6,10 @@ class Settings(BaseSettings):
     DB_USER: str
     DB_PASS: str
     DB_NAME: str
+    JWT_SECRET_KEY: str
+    JWT_ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    REFRESH_TOKEN_EXPIRE_DAYS: int
 
 
     @property
