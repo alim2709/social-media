eedb796767402803155467352e2a4edd6d6ae065
alim2709 <alimselemetow@gmail.com>
2024-11-03 23:06:59 +0100

    Include comment_router into app

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ from typing import Union
 from fastapi import FastAPI
 from app.users.routers import auth_router
 from app.posts.routers import post_router
+from app.comments.routers import comment_router
 
 app = FastAPI()
 
 app.include_router(auth_router)
 app.include_router(post_router)
+app.include_router(comment_router)
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
