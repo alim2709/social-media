0041cf08af94268f0cb8714f46cbcfa115201ef5
alim2709 <alimselemetow@gmail.com>
2024-11-04 15:12:13 +0100

    Refactor HTTPBearer in dependencies.py file

--- app/users/dependencies.py
+++ app/users/dependencies.py
@@ -6,7 +6,7 @@ from fastapi.exceptions import HTTPException
 from app.users.auth import decode_token
 
 
-class AccessTokenBearer(HTTPBearer):
+class TokenBearer(HTTPBearer):
     def __init__(self, auto_error: bool = True):
         super().__init__(auto_error=auto_error)
 
@@ -23,11 +23,7 @@ class AccessTokenBearer(HTTPBearer):
                 detail="Token expired or invalid"
             )
 
-        if token_data["refresh"]:
-            raise HTTPException(
-                status_code=status.HTTP_403_FORBIDDEN,
-                detail="Please provide an access token"
-            )
+        self.verify_token_data(token_data)
 
         return token_data
 
@@ -36,3 +32,25 @@ class AccessTokenBearer(HTTPBearer):
         token_data = decode_token(token)
 
         return True if token_data else False
+
+    def verify_token_data(self, token_data: dict):
+        raise NotImplementedError("Please Implement this method")
+
+
+class AccessTokenBearer(TokenBearer):
+
+    def verify_token_data(self, token_data: dict) -> None:
+        if token_data and token_data["refresh"]:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="Please provide an access token"
+            )
+
+class RefreshTokenBearer(TokenBearer):
+
+    def verify_token_data(self, token_data: dict) -> None:
+        if token_data and not token_data["refresh"]:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="Please provide an refresh token"
+            )
