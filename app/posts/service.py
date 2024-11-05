818eeb0d93157d45c88b7bffc007375d289b4547
alim2709 <alimselemetow@gmail.com>
2024-11-05 12:39:38 +0100

    Refactor PostService class

--- app/posts/service.py
+++ app/posts/service.py
@@ -5,36 +5,39 @@ from app.models import Post
 
 
 class PostService:
-    session = async_session_maker()
+    def __init__(self, session=async_session_maker):
+        self.session = session
 
-    async def get_posts(self):
-        async with self.session as session:
+    async def get_posts(self, filter_data = None):
+        async with self.session() as session:
             query = select(Post)
+            if filter_data.is_blocked or not filter_data.is_blocked:
+                query = query.where(Post.is_blocked == filter_data.is_blocked)
             result = await session.execute(query)
             return result.scalars().all()
 
     async def get_post_by_id(self, post_id):
-        async with self.session as session:
+        async with self.session() as session:
             query = select(Post).where(Post.id == post_id)
             result = await session.execute(query)
 
             return result.scalar_one_or_none()
 
     async def get_post_by_title(self, title: str):
-        async with self.session as session:
+        async with self.session() as session:
             query = select(Post).where(Post.title == title)
             result = await session.execute(query)
             return result.scalar_one_or_none()
 
     async def create_post(self, **post_data):
-        async with self.session as session:
+        async with self.session() as session:
             query = insert(Post).values(**post_data).returning(Post)
             result = await session.execute(query)
             await session.commit()
             return result.scalar_one()
 
     async def update_post(self, post_id, **post_data):
-        async with self.session as session:
+        async with self.session() as session:
             query = update(Post).values(**post_data).where(Post.id == post_id)
             result = await session.execute(query)
             await session.commit()
@@ -46,7 +49,7 @@ class PostService:
             return updated_post
 
     async def delete_post(self, post_id):
-        async with self.session as session:
+        async with self.session() as session:
             query = delete(Post).where(Post.id == post_id)
             result = await session.execute(query)
             await session.commit()
