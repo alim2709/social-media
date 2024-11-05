d0b4c29742b320e9aab6b257f91dc90d66963ab8
alim2709 <alimselemetow@gmail.com>
2024-11-05 15:42:35 +0100

    Add SPostDetailModel pydantic model at comments/posts.py file

--- app/posts/schemas.py
+++ app/posts/schemas.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
+
+from app.comments.schemas import SCommentPostDetailModel
 
 
 class SPostModel(BaseModel):
@@ -10,6 +12,18 @@ class SPostModel(BaseModel):
     created_at: datetime
     is_blocked: bool
     author_id: int
+
+class SPostDetailModel(BaseModel):
+    id: int
+    title: str
+    content: str
+    created_at: datetime
+    is_blocked: bool
+    author_id: int
+
+    comments: list[SCommentPostDetailModel] = []
+
+    model_config = ConfigDict(from_attributes=True)
 
 
 class SPostCreateModel(BaseModel):
