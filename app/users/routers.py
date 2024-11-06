75983611da3b25172db2067dbf197243f8f7b4ea
alim2709 <alimselemetow@gmail.com>
2024-11-06 09:45:33 +0100

    Implement enable_auto_reply endpoint at users/routers.py file

--- app/users/routers.py
+++ app/users/routers.py
@@ -2,6 +2,7 @@ from datetime import timedelta, datetime
 
 from app.users.auth import get_password_hash, verify_password, create_access_token
 from app.config import settings
+from app.posts.routers import access_token_bearer
 from fastapi import APIRouter, HTTPException, status, Depends
 from fastapi.responses import JSONResponse
 from app.users.service import UserService
@@ -106,6 +107,18 @@ async def get_new_access_token(token_details:dict = Depends(refresh_token_servic
         detail = "Token invalid or expired"
     )
 
-@auth_router.post("/user/{user_id}/enable_auto_reply")
-async def enable_auto_reply(user_id, user_data: SUserEnableAutoReplyModel = Depends()):
-    pass
+@auth_router.post("/user/{user_id}/enable_auto_reply", response_model=SUserModel)
+async def enable_auto_reply(
+        user_id,
+        user_data_to_update: SUserEnableAutoReplyModel = Depends(),
+        user_details = Depends(access_token_bearer)):
+    user_data_to_update_dict = user_data_to_update.model_dump()
+    current_user_id = int(user_details["user"]["id"])
+    if current_user_id != int(user_id):
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not enough permissions")
+
+    updated_user = await user_service.update_user(user_id=int(user_id), **user_data_to_update_dict)
+
+    return updated_user
+
+
