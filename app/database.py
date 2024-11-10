3aa97a9e30d9990cb2dab0ce97a6499ce82fac62
alim2709 <alimselemetow@gmail.com>
2024-11-11 00:17:20 +0100

    Update database.py file

--- app/database.py
+++ app/database.py
@@ -1,12 +1,17 @@
+from sqlalchemy import NullPool
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from app.config import settings
 from contextlib import asynccontextmanager
 
+if settings.MODE == "TEST":
+    DATABASE_URL = settings.TEST_DATABASE_URL
+    DATABASE_PARAMS = {"poolclass": NullPool}
+else:
+    DATABASE_URL = settings.DATABASE_URL
+    DATABASE_PARAMS = {}
 
-DATABASE_URL = settings.DATABASE_URL
-
-engine = create_async_engine(DATABASE_URL)
+engine = create_async_engine(DATABASE_URL, **DATABASE_PARAMS)
 
 async_session_maker = sessionmaker(engine, class_= AsyncSession, expire_on_commit=False)
 
